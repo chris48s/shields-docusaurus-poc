fef502c8b085a52f0667cf5c4902746d9beda09f
chris48s <git@chris-shaw.dev>
2022-12-11 19:41:13 +0000

    mock up quick 'community' page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,6 +64,7 @@ const config = {
           },
           { to: "/badges", label: "Badges", position: "left" },
           { to: "/blog", label: "Blog", position: "left" },
+          { to: "/community", label: "Community", position: "left" },
           {
             href: "https://github.com/badges/shields",
             label: "GitHub",
